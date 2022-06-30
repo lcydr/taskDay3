2d8604b1dfee32a30224de7397a5b367ec56c42e
wgs66 <707205590@qq.com>
2022-06-30 20:53:26 +0800

    动态绑定class

---
+++ src/动态class绑定.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p v-bind:class="{ active: isShow }" class="ppp">我是乒乒乓乓</p>
+    <button @click="isShow = !isShow">Click ME</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+.active {
+  color: red;
+}
+.ppp {
+  border: 10px solid #ccc;
+}
+</style>
