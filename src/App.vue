946a9d4da7d9bd2d722ba5d61a73b3766a5a0167
lcydr <1211753823@qq.com>
2022-06-30 19:46:50 +0800

    动态绑定class

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,34 @@
 <template>
   <div>
-        <ul>
-      <li v-for="(val, index) in arr" :key="index">
-        {{ val }}
-      </li>
-    </ul>
-    <button @click="revBtn">数组翻转</button>
-    <button @click="sliceBtn">截取前3个</button>
-    <button @click="updateBtn">更新第一个元素值</button>
+    <p v-bind:class="{active: activeFlag}" class="ppp">我是乒乒乓乓</p>
+    <button @click="activeFlag = !activeFlag">Click ME</button>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  name: 'VuecliDemo03Class',
+
+  data() {
     return {
-      arr:[1,2,3,4,5]
-    }
+      activeFlag: true
+    };
   },
-  methods:{
-    revBtn(){ 
-      this.arr.reverse()
-    },
-    sliceBtn(){
-      this.arr.splice(3)
-    },
-    updateBtn(){
-      this.$set(this.arr,0,100)
-    }
+
+  mounted() {
+    
   },
-}
+
+  methods: {
+    
+  },
+};
 </script>
-
 <style>
-
+  .active {
+    color: red;
+  }
+  .ppp{
+    border: 10px solid #ccc;
+  }
 </style>
