6a7701dd4e48d8959d24b7806717a17be0c48e88
wgs66 <707205590@qq.com>
2022-06-30 20:35:25 +0800

    v-for使用

---
+++ src/v-for使用.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="obj in arr" :key="obj">{{ obj }}</li>
+    </ul>
+    <ul>
+      <li v-for="obj in stuArr" :key="obj.id">
+        {{ obj.id }}-{{ obj.name }}-{{ obj.hobby }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(obj, index) in tObj" :key="index">{{ obj }}-{{ index }}</li>
+      <li v-for="obj in count" :key="obj">{{ obj }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: ['小明', '小欢欢', '大黄'],
+      stuArr: [
+        {
+          id: 1001,
+          name: '孙悟空',
+          sex: '男',
+          hobby: '吃桃子',
+        },
+        {
+          id: 1002,
+          name: '猪八戒',
+          sex: '男',
+          hobby: '背媳妇',
+        },
+      ],
+      tObj: {
+        name: '小黑',
+        age: 18,
+        class: '1期',
+      },
+      count: 10,
+    };
+  },
+};
+</script>
+
+<style></style>
