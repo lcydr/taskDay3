301473f5cc7893ca93956e27de3551df26e4c2b3
wgs66 <707205590@qq.com>
2022-06-30 19:45:31 +0800

    v-if与v-show的区别

---
+++ src/v-if与v-show的区别.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- v-show通过更改style样式实现隐藏与显示 -->
+    <p v-show="isShow">我是v-show</p>
+    <button @click="btn">显示隐藏</button>
+
+    <br />
+    <br />
+    <!-- v-if会false会销毁dom元素 -->
+    <p v-if="isShow1">我是v-if</p>
+    <button @click="btn1">显示隐藏</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+      isShow1: false,
+    };
+  },
+  methods: {
+    btn() {
+      this.isShow = !this.isShow;
+    },
+    btn1() {
+      this.isShow1 = !this.isShow1;
+    },
+  },
+};
+</script>
+
+<style></style>
