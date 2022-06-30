d7257e76e5dc288f2828886e280c5c12099d89b0
lcydr <1211753823@qq.com>
2022-06-30 19:56:56 +0800

    案例练习

---
+++ src/3.动态绑定class.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p v-bind:class="{active: activeFlag}" class="ppp">我是乒乒乓乓</p>
+    <button @click="activeFlag = !activeFlag">Click ME</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VuecliDemo03Class',
+
+  data() {
+    return {
+      activeFlag: true
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+<style>
+  .active {
+    color: red;
+  }
+  .ppp{
+    border: 10px solid #ccc;
+  }
+</style>
