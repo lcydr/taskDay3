bad07e6e93060d29ae7629a52293354da1da104e
wgs66 <707205590@qq.com>
2022-06-30 19:50:58 +0800

    v-if与v-if-else

---
+++ src/v-if与v-if-else.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p v-if="age >= 18">成年了</p>
+    <p v-else-if="age < 18 && age >= 12">少年</p>
+    <p v-else>儿童</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      age: 10,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
