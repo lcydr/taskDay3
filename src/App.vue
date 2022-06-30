c2f2fc931caffd96ec853e17ed42adedb7daf02c
wgs66 <707205590@qq.com>
2022-06-30 19:23:02 +0800

    v-text和v-html区别

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app"></div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  data() {
+    return {};
+  },
 };
 </script>
 
---
+++ src/v-text和v-html区别.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ str }}</h1>
+
+    <!-- 读取html标签 -->
+    <h2 v-html="str"></h2>
+    <!-- 只读取文本 -->
+    <h2 v-text="str"></h2>
+
+    <!-- v-html会覆盖插值表达式 -->
+    <h2 v-html="str">{{ 123 }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      str: '<span>123465</span>',
+    };
+  },
+};
+</script>
+
+<style></style>
