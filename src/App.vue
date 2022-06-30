cb5eae43ff22ab3b130e9fd9551952016d127d6a
lcydr <1211753823@qq.com>
2022-06-30 21:24:30 +0800

    作业学生管理系统

--- src/App.vue
+++ src/App.vue
@@ -1,169 +1,96 @@
 <template>
-  <div class="wrap">
-    <div class="nav_left" id="navLeft">
-      <div class="nav_content">
-        <span v-for="(item,index) in arr" :key="index" @click="changeFont(index)" :class="[index == num ? 'active' : '']">{{item.first_name}}</span>
-      </div>
+  <div id="app">
+    <div>
+      <span>姓名:</span>
+      <input type="text" v-model="name" />
     </div>
-    <div class="down">
-      <i class="iconfont icon-xiajiantoubeifen gray"></i>
+    <div>
+      <span>年龄:</span>
+      <input type="number" v-model="age" />
+    </div>
+    <div>
+      <span>性别:</span>
+      <select v-model="sex" @change="getSex">
+        <option :value="obj.id" v-for="obj in list" :key="obj.id">{{obj.name}}</option>
+      </select>
+    </div>
+    <div>
+      <button @click="add" >添加/修改</button>
+    </div>
+    <div>
+      <table
+        border="1"
+        cellpadding="10"
+        cellspacing="0"
+      >
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="(item, index) in arr" :key="index">
+          <td>{{index +1}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.age}}</td>
+          <td>{{item.sex}}</td>
+          <td>
+            <button @click="btnDelete(index)">删除</button>
+            <button @click="btnEdit(index)">编辑</button>
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
-
 <script>
 export default {
-  data() {
+  data(){
     return {
+      name:'',
+      age:0,
       arr: [
-        {
-          first_id: "0",
-          first_name: "热门"
-        },
-        {
-          first_id: "621",
-          first_name: "\u5496\u5561",
-        },
-        {
-          first_id: "627",
-          first_name: "\u996e\u98df",
-        },
-        {
-          first_id: "279",
-          first_name: "\u7537\u88c5",
-        },
-        {
-          first_id: "294",
-          first_name: "\u5973\u88c5",
-        },
-        {
-          first_id: "122",
-          first_name: "\u773c\u955c",
-        },
-        {
-          first_id: "339",
-          first_name: "\u5185\u8863\u914d\u9970",
-        },
-        {
-          first_id: "391",
-          first_name: "\u6bcd\u5a74",
-        },
-        {
-          first_id: "35",
-          first_name: "\u978b\u9774",
-        },
-        {
-          first_id: "39",
-          first_name: "\u8fd0\u52a8",
-        },
-        {
-          first_id: "153",
-          first_name: "\u7bb1\u5305",
-        },
-        {
-          first_id: "119",
-          first_name: "\u7f8e\u5986\u4e2a\u62a4",
-        },
-        {
-          first_id: "355",
-          first_name: "\u5bb6\u7eba",
-        },
-        {
-          first_id: "51",
-          first_name: "\u9910\u53a8",
-        },
-        {
-          first_id: "334",
-          first_name: "\u7535\u5668",
-        },
-        {
-          first_id: "369",
-          first_name: "\u5bb6\u88c5",
-        },
-        {
-          first_id: "10",
-          first_name: "\u5bb6\u5177",
-        },
-        {
-          first_id: "223",
-          first_name: "\u6570\u7801",
-        },
-        {
-          first_id: "429",
-          first_name: "\u6c7d\u914d",
-        },
-        {
-          first_id: "546",
-          first_name: "\u5065\u5eb7\u4fdd\u5065",
-        },
-        {
-          first_id: "433",
-          first_name: "\u5b9a\u5236",
-        },
+        { name:'张三',age:10,sex:'男'},
+        { name:'李四',age:20,sex:'女'}
       ],
-      num:0
-    };
+      list: [
+        {id:'男',name:'男'},
+        {id:'女',name:'女'}
+      ],
+    }
   },
+  created(){
+      this.sex = this.list[0].id
+    },
+    // 获取select选择
   methods:{
-    changeFont(index){
-      this.num = index
-      console.log(index)
-      console.log(this.num)
+    getSex(){
+      console.log(this.sex)
+    },
+    // 添加
+    add(){
+      if(this.name.trim().length === 0 || this.age === 0) return alert('请输入完整信息')
+    this.arr.push({
+      index: this.index+1,
+      name: this.name, 
+      age: this.age,
+      sex: this.sex,
+    })
+      this.name=""
+      this.age=0
+    },
+    btnDelete(ind){
+    //  let index = this.arr.findIndex(obj => obj.index == id);
+     this.arr.splice(ind,1)
+    },
+    btnEdit(index){
+
+      this.name =this.arr[index].name
+      this.age =this.arr[index].age
+      this.sex = this.arr[index].age
+      add()
     }
   }
-};
+}
 </script>
-
-<style>
-.wrap {
-  width: 100%;
-  display: flex;
-  margin: 0.2rem 0 0 0;
-  position: relative;
-}
-
-/*左侧的导航样式*/
-.nav_left {
-  width: 21.1875rem;
-  overflow: scroll;
-}
-
-.nav_left::-webkit-scrollbar {
-  display: none;
-}
-
-.nav_content {
-  white-space: nowrap;
-  padding: 0 0.7rem;
-}
-
-.nav_content span {
-  display: inline-block;
-  padding: 0.4rem 0.6rem;
-  font-size: 0.875rem;
-}
-
-.nav_content .active {
-  border-bottom: 2px solid #7f4395;
-  color: #7f4395;
-}
-
-.nav_left,
-.down {
-  float: left;
-}
-
-/*右侧导航部分*/
-.down {
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.gray {
-  color: gray;
-  display: inline-block;
-  vertical-align: middle;
-}
-</style>
