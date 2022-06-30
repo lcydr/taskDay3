7b0d5a262ec1c061a7892606a0cc75e8a0939556
lcydr <1211753823@qq.com>
2022-06-30 20:39:25 +0800

    附加练习2

---
+++ src/6.附加练习-1.帅哥美女走一走.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in arr" :key="item" >{{item}}</li>
+    </ul>
+    <button @click="btn">走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      arr:['1','2','3'] 
+    }
+    
+  },
+  methods:{
+    btn(){
+      this.arr.push(this.arr[0])
+      this.arr.shift()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
