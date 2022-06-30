c394aac6e0795bd19556411e07469a01c0787925
偏执 <1924618143@qq.com>
2022-06-30 19:05:37 +0800

    创建

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ str }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      str: 'asdsadfsadfasfsafas',
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
