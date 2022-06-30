837a8f8377c16946ad815130f335bed4b62f4327
wlt <734490921@qq.com>
2022-06-30 20:51:51 +0800

    init project

---
+++ src/05_动态style.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p v-bind:style="{ color: 'aqua', fontSize: '123px' }">adsfsgdfs</p>
+
+    <p v-bind:style="{ color: colorColor, fontSize: '143px' }">adsfsgdfsdfs</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colorColor: 'red',
+    }
+  },
+}
+</script>
+
+<style></style>
