e830df3307bcce857010bdf08def42f642242568
wgs66 <707205590@qq.com>
2022-06-30 20:55:48 +0800

    动态绑定style

---
+++ src/动态绑定style.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p :style="{ color: color, fontSize: fontSize }">123345</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: 'red',
+      fontSize: '100px',
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
