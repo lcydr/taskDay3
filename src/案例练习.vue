b7a3a3c6702923e7f599423e2c933342f4f8f3bd
wgs66 <707205590@qq.com>
2022-06-30 21:08:12 +0800

    案例练习

---
+++ src/案例练习.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ul>
+      <li
+        :class="{ active: index === index1 }"
+        v-for="(obj, index) in navs"
+        :key="index"
+        @click="btn(index)"
+      >
+        {{ obj }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      index1: 0,
+      navs: ['大学起点', '高中起点', '初中起点', '小学起点'],
+    };
+  },
+  methods: {
+    btn(index) {
+      this.index1 = index;
+      console.log(index);
+    },
+  },
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  border-radius: 10px;
+  width: 400px;
+  overflow: hidden;
+  padding: 0;
+}
+ul li {
+  float: left;
+  width: 100px;
+  height: 40px;
+  background-color: #ccc;
+  color: #fff;
+  text-align: center;
+  line-height: 40px;
+  cursor: pointer;
+}
+li.active {
+  background-color: blue;
+}
+</style>
